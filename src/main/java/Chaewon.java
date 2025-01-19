ae663e5ee2e982748b94a8360020335e9d576559
bipplane <ryanchen@live.com.sg>
2025-01-19 23:15:21 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Chaewon.java
@@ -0,0 +1,15 @@
+public class Chaewon {
+    public static void main(String[] args) {
+        Chaewon.printUnderscore();
+        System.out.println("Hello! I'm Kim Chaewon!\n"
+                + "What can I do for you?");
+        Chaewon.printUnderscore();
+        System.out.println("Get an A for this mod and" +
+                " I'll reunite IZ*ONE for you <3");
+        Chaewon.printUnderscore();
+    }
+    // Method to print a line of underscores for my lazy ahh
+    public static void printUnderscore() {
+        System.out.println("____________________________________________________________");
+    }
+}
