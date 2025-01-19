18ca2ee208e8b2af84f283b105725329e68b75e9
bipplane <ryanchen@live.com.sg>
2025-01-19 23:31:08 +0800

    Level-1: Echo

--- src/main/java/Chaewon.java
+++ src/main/java/Chaewon.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Chaewon {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Chaewon.printUnderscore();
         System.out.println("Hello! I'm Kim Chaewon!\n"
                 + "What can I do for you?");
+        Chaewon.printUnderscore();
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                Chaewon.printUnderscore();
+                System.out.println(input);
+                Chaewon.printUnderscore();
+            }
+        }
+
         Chaewon.printUnderscore();
         System.out.println("Get an A for this mod and" +
                 " I'll reunite IZ*ONE for you <3");
