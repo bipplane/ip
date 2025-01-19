30a9d4da4bcb386e39eea299abd6298c00449be0
bipplane <ryanchen@live.com.sg>
2025-01-19 23:51:45 +0800

    Level-2: Add, List

--- src/main/java/Chaewon.java
+++ src/main/java/Chaewon.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Chaewon {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
         Chaewon.printUnderscore();
         System.out.println("Hello! I'm Kim Chaewon!\n"
                 + "What can I do for you?");
@@ -12,15 +14,22 @@ public class Chaewon {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                Chaewon.printUnderscore();
+                for (int i = 0; i != list.size(); i++) {
+                    System.out.println(i + 1 + ". " + list.get(i));
+                }
+                Chaewon.printUnderscore();
             } else {
                 Chaewon.printUnderscore();
-                System.out.println(input);
+                list.add(input);
+                System.out.println("added: " + input);
                 Chaewon.printUnderscore();
             }
         }
 
         Chaewon.printUnderscore();
-        System.out.println("Get an A for this mod and" +
+        System.out.println("Bye! Get an A for this mod and" +
                 " I'll reunite IZ*ONE for you <3");
         Chaewon.printUnderscore();
     }
