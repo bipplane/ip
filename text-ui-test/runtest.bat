7bf9b6a37d0619ef8060472464f1d7f22ff4f22a
bipplane <ryanchen@live.com.sg>
2025-01-29 16:46:36 +0800

    Update runtest

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,8 +3,9 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
-REM delete output from previous run
+REM delete output and tasks from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
+if exist tasks.txt delete tasks.txt
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
