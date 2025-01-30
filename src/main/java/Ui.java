6219d50d89732af999d4f071e2f499d159d8876d
bipplane <ryanchen@live.com.sg>
2025-01-30 14:26:09 +0800

    Add Ui class to deal with user interactions

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,5 @@
+import tasks.*;
+
 import java.util.Scanner;
 
 public class Ui {
@@ -7,30 +9,19 @@ public class Ui {
     }
 
     /**
-     * Prints a line of underscores to the console.
-     */
-    public static void printLine() {
-        System.out.println("____________________________________________________________");
-    }
-
-    /**
      * Prints a welcome message to the console.
      */
     public static void welcome() {
-        printLine();
         System.out.println("Hello! I'm Kim Chaewon!");
         System.out.println("What can I do for you?");
-        printLine();
     }
 
     /**
      * Prints a goodbye message to the console.
      */
     public static void goodbye() {
-        printLine();
         System.out.println("Bye! Get an A for this mod and "
                 + "I'll reunite IZ*ONE for you <3");
-        printLine();
     }
 
     /**
@@ -39,8 +30,28 @@ public class Ui {
      * @param message The message to be printed.
      */
     public static void printMessage(String message) {
-        printLine();
         System.out.println(message);
-        printLine();
+    }
+
+    public static <T extends Task> void markTask(T task) {
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(task.toString());
+    }
+
+    public static <T extends Task> void unmarkTask(T task) {
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(task.toString());
+    }
+
+    public static <T extends Task> void addTask(T task, int size) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(task.toString());
+        System.out.println("Now you have " + size + " tasks in the list.");
+    }
+
+    public static <T extends Task> void deleteTask(T task, int size) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(task.toString());
+        System.out.println("Now you have " + size + " tasks in the list.");
     }
 }
