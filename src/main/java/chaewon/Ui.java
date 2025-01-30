155f392b548cda20958e6ca8ea88765b83d8e1c1
bipplane <ryanchen@live.com.sg>
2025-01-30 21:43:29 +0800

    Add Ui class to deal with user interactions

--- src/main/java/chaewon/Ui.java
+++ src/main/java/chaewon/Ui.java
@@ -1,25 +1,26 @@
+package chaewon;
+
 import tasks.*;
 
-import java.util.Scanner;
-
 public class Ui {
-    private final Scanner scanner = new Scanner(System.in);
-
-    public Ui() {
-    }
 
     /**
      * Prints a welcome message to the console.
      */
-    public static void welcome() {
+    public void welcome() {
         System.out.println("Hello! I'm Kim Chaewon!");
         System.out.println("What can I do for you?");
+    }
+
+    public void hello() {
+        System.out.println("Hello! I'm Chaewon, " +
+                "leader of South Korean girl group Le Sserafim!");
     }
 
     /**
      * Prints a goodbye message to the console.
      */
-    public static void goodbye() {
+    public void goodbye() {
         System.out.println("Bye! Get an A for this mod and "
                 + "I'll reunite IZ*ONE for you <3");
     }
@@ -29,29 +30,40 @@ public class Ui {
      *
      * @param message The message to be printed.
      */
-    public static void printMessage(String message) {
+    public void printMessage(String message) {
         System.out.println(message);
     }
 
-    public static <T extends Task> void markTask(T task) {
+    public <T extends Task> void markedTask(T task) {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(task.toString());
     }
 
-    public static <T extends Task> void unmarkTask(T task) {
+    public <T extends Task> void unmarkedTask(T task) {
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(task.toString());
     }
 
-    public static <T extends Task> void addTask(T task, int size) {
+    public <T extends Task> void addedTask(T task, int size) {
         System.out.println("Got it. I've added this task:");
         System.out.println(task.toString());
         System.out.println("Now you have " + size + " tasks in the list.");
     }
 
-    public static <T extends Task> void deleteTask(T task, int size) {
+    public <T extends Task> void deletedTask(T task, int size) {
         System.out.println("Noted. I've removed this task:");
         System.out.println(task.toString());
         System.out.println("Now you have " + size + " tasks in the list.");
+    }
+
+    public void showTasks(TaskList tasks) {
+        if (tasks.getNumTasks() == 0) {
+            System.out.println("You have no tasks in your list. Slay!");
+            return;
+        }
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.getNumTasks(); i++) {
+            System.out.println((i + 1) + ". " + tasks.getTasks().get(i).toString());
+        }
     }
 }
