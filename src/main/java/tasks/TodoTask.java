ea9d39151718f4370371a731b367edb06d9fd195
bipplane <ryanchen@live.com.sg>
2025-01-31 19:41:08 +0800

    Add Javadoc comments to Task and its subclasses

--- src/main/java/tasks/TodoTask.java
+++ src/main/java/tasks/TodoTask.java
@@ -1,11 +1,25 @@
 package tasks;
 
-// Description: A class that represents a ToDo task.
+/**
+ * Represents a todo task.
+ */
 public class TodoTask extends Task {
+
+    /**
+     * Constructor for TodoTask.
+     *
+     * @param taskName Name of the task.
+     */
     public TodoTask(String taskName) {
         super(taskName);
     }
 
+    /**
+     * Constructor for TodoTask.
+     *
+     * @param taskName Name of the task.
+     * @param isDone Status of the task.
+     */
     public TodoTask(String taskName, boolean isDone) {
         super(taskName, isDone);
     }
