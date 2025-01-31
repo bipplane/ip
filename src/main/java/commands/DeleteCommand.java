030b4d82c7031a860ec18937e5b61f38d94b1361
bipplane <ryanchen@live.com.sg>
2025-01-31 19:50:58 +0800

    Add Javadoc comments to Command and its subclasses

--- src/main/java/commands/DeleteCommand.java
+++ src/main/java/commands/DeleteCommand.java
@@ -1,11 +1,22 @@
 package commands;
 
-import chaewon.*;
-import tasks.*;
+import chaewon.ChaewonException;
+import chaewon.Storage;
+import chaewon.TaskList;
+import chaewon.Ui;
+import tasks.Task;
 
+/**
+ * Represents a command to delete a task.
+ */
 public class DeleteCommand extends Command {
     private final int taskIndex;
 
+    /**
+     * Constructor for DeleteCommand.
+     *
+     * @param taskIndex The index of the task to delete.
+     */
     public DeleteCommand(int taskIndex) {
         this.taskIndex = taskIndex;
     }
