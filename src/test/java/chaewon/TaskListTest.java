9fc90fdb788180c11bd5c28c4c04e9b76c675f5e
bipplane <ryanchen@live.com.sg>
2025-02-01 19:25:19 +0800

    Use checkstyle

--- src/test/java/chaewon/TaskListTest.java
+++ src/test/java/chaewon/TaskListTest.java
@@ -6,17 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import tasks.*;
+import tasks.Task;
+import tasks.TodoTask;
 
+/**
+ * Represents a task list test.
+ */
 public class TaskListTest {
 
     private TaskList taskList;
 
+    /**
+     * Sets up the task list for testing.
+     */
     @BeforeEach
     public void setUp() {
         taskList = new TaskList();
     }
 
+    /**
+     * Tests the addTask method.
+     */
     @Test
     public void addTask_todoTask_success() {
         Task task = new TodoTask("Test todo task");
@@ -25,12 +35,18 @@ public class TaskListTest {
         assertEquals(task, taskList.getTasks().get(0));
     }
 
+    /**
+     * Tests the addTask method with a null task.
+     */
     @Test
     public void addTask_nullTask_exceptionThrown() {
         ChaewonException e = assertThrows(ChaewonException.class, () -> taskList.addTask(null));
         assertEquals("Gurl what? Task cannot be null.", e.getMessage());
     }
 
+    /**
+     * Tests the removeTask method.
+     */
     @Test
     public void removeTask_todoTask_success() {
         Task task = new TodoTask("Test task");
@@ -39,12 +55,18 @@ public class TaskListTest {
         assertEquals(0, taskList.getNumTasks());
     }
 
+    /**
+     * Tests the removeTask method with an invalid index.
+     */
     @Test
     public void removeTask_invalidIndex_exceptionThrown() {
         ChaewonException e = assertThrows(ChaewonException.class, () -> taskList.removeTask(0));
         assertEquals("Invalid task number. Please try again.", e.getMessage());
     }
 
+    /**
+     * Tests the getTask method.
+     */
     @Test
     public void testGetTask() {
         Task task = new TodoTask("Test task");
@@ -53,6 +75,9 @@ public class TaskListTest {
         assertEquals(task, retrievedTask);
     }
 
+    /**
+     * Tests the getNumTasks method.
+     */
     @Test
     public void testSize() {
         assertEquals(0, taskList.getNumTasks());
